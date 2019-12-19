dd4bcfac21ecf3f8597efc51508ae0c9369b3666
Ahmed Belgacem <ahmedbelgaacem@gmail.com>
2019-12-19 10:45:56 +0100

    prepared the architecture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,40 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { SignUpComponent } from "src/app/auth/sign-up/sign-up.component";
+import { SignInComponent } from "src/app/auth/sign-in/sign-in.component";
+import { AuthComponent } from "src/app/auth/auth.component";
+import { EditAlbumComponent } from "src/app/profil/edit-album/edit-album.component";
+import { AboutComponent } from "src/app/about/about.component";
+import { ProfilComponent } from "src/app/profil/profil.component";
+import { MainComponent } from "src/app/containers/main/main.component";
+import { HomeComponent } from "src/app/home/home.component";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: MainComponent,
+    children: [
+      { path: "home", component: HomeComponent },
+      { path: "profil", component: ProfilComponent },
+      { path: "profil/edit-album", component: EditAlbumComponent },
+      { path: "profil/add-album", component: EditAlbumComponent },
+      { path: "about", component: AboutComponent },
+      { path: "", redirectTo: "home", pathMatch: "full" }
+    ]
+  },
+  {
+    path: "auth",
+    component: AuthComponent,
+    children: [
+      { path: "sign-in", component: SignInComponent },
+      { path: "sign-up", component: SignUpComponent },
+      { path: "", redirectTo: "sign-in", pathMatch: "full" }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { OverviewComponent } from './home/overview/overview.component';
+import { FilterComponent } from './home/overview/components/filter/filter.component';
+import { InfiniteScrollComponent } from './home/overview/components/infinite-scroll/infinite-scroll.component';
+import { ProfilComponent } from './profil/profil.component';
+import { AuthComponent } from './auth/auth.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { MainComponent } from './containers/main/main.component';
+import { HeaderComponent } from './containers/header/header.component';
+import { FooterComponent } from './containers/footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { EditAlbumComponent } from './profil/edit-album/edit-album.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OverviewComponent,
+    FilterComponent,
+    InfiniteScrollComponent,
+    ProfilComponent,
+    AuthComponent,
+    SignUpComponent,
+    SignInComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    EditAlbumComponent
   ],
   imports: [
     BrowserModule,
